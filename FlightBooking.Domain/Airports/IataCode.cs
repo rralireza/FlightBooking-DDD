36f05f586da667533396b33cef86429effcb995b
Alireza Nikandish <rralireza254@yahoo.com>
2025-11-24 18:36:00 +0330

    Modified IataCode value object

--- FlightBooking.Domain/Airports/IataCode.cs
+++ FlightBooking.Domain/Airports/IataCode.cs
@@ -2,39 +2,16 @@
 
 public sealed record IataCode
 {
-    internal static readonly IataCode Unknown = new("Unknown");
-    public static readonly IataCode TehranMehrAbad = new("THR");
-    public static readonly IataCode TehranImamKhomeini = new("IKA");
-    public static readonly IataCode Ahwaz = new("AWZ");
-    public static readonly IataCode Isfahan = new("IFN");
-    public static readonly IataCode Shiraz = new("SYZ");
-    public static readonly IataCode Mashhad = new("MHD");
-    public static readonly IataCode Tabriz = new("TBZ");
-    public static readonly IataCode Kish = new("KIH");
-    public static readonly IataCode Istanbul = new("IST");
-    public static readonly IataCode Ankara = new("ESB");
-    public static readonly IataCode London = new("LHR");
-
-    private IataCode(string code) => Code = code;
-
-    public string Code { get; set; }
-
-    public static IataCode FindIataCode(string code) =>
-        AllCodes.FirstOrDefault(x => x.Code == code) ?? throw new ApplicationException("Invalid IATA code.");
-
-    public static readonly IReadOnlyList<IataCode> AllCodes = new List<IataCode>()
+    private IataCode(string code)
     {
-        Unknown,
-        TehranMehrAbad,
-        TehranImamKhomeini,
-        Ahwaz,
-        Isfahan,
-        Shiraz,
-        Mashhad,
-        Tabriz,
-        Kish,
-        Istanbul,
-        Ankara,
-        London
-    }.AsReadOnly();
+        if (string.IsNullOrWhiteSpace(code))
+            throw new ArgumentException("IATA code cannot be empty or null!", nameof(code));
+
+        if (code.Length != 3 || !code.All(char.IsUpper))
+            throw new ArgumentException("IATA code must be exactly 3 uppercase letters!", nameof(code));
+    }
+
+    public string Code { get; init; }
+
+    public static IataCode Create(string code) => new(code);
 }
