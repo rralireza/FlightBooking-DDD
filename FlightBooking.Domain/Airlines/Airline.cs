4b7a1a9711ba97370c1d38a99073e9a463c0e5e5
Alireza Nikandish <rralireza254@yahoo.com>
2025-12-09 20:49:26 +0330

    add initial Airline entity

---
+++ FlightBooking.Domain/Airlines/Airline.cs
@@ -0,0 +1,16 @@
+﻿using FlightBooking.Domain.Abstractions;
+
+namespace FlightBooking.Domain.Airlines;
+
+public sealed class Airline : Entity<int>
+{
+    private Airline(int id) : base(id) { }
+
+    private Airline() { }
+
+    public string IataCode { get; private set; }
+
+    public string Name { get; private set; }
+
+    public bool IsActive { get; private set; } = true;
+}
