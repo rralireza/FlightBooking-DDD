e54531d13c074123d27a174831c964740eb65b84
Alireza Nikandish <rralireza254@yahoo.com>
2025-12-18 11:01:44 +0330

    implement factory method into Reservation entity and declare Reservation value objects

---
+++ FlightBooking.Domain/Reservations/Money.cs
@@ -0,0 +1,29 @@
+﻿namespace FlightBooking.Domain.Reservations;
+
+public sealed record Money
+{
+    public decimal Amount { get; init; }
+    public string Currency { get; init; } = string.Empty;
+
+    private Money(decimal amount, string currency)
+    {
+        Amount = amount;
+        Currency = currency;
+    }
+
+    public static Money Zero() => Create(0m, "None");
+
+    public static Money Create(decimal amount, string currency)
+    {
+        if (amount < 0)
+            throw new ApplicationException("Amount cannot be negative!");
+
+        if (string.IsNullOrWhiteSpace(currency))
+            throw new ArgumentNullException("Currency cannot be empty!");
+
+        if (currency.Length != 3)
+            throw new ArgumentException("Currency must be a 3-letter ISO code!");
+
+        return new Money(amount, currency.ToUpperInvariant());
+    }
+}
--- FlightBooking.Domain/Reservations/Reservation.cs
+++ FlightBooking.Domain/Reservations/Reservation.cs
@@ -6,24 +6,45 @@ namespace FlightBooking.Domain.Reservations;
 
 public sealed class Reservation : Entity<Guid>
 {
-    private Reservation(Guid id) : base(id)
+    private Reservation(Guid id, long flightId, string userId, DateTime expiresAt) : base(id)
     {
+        FlightId = flightId;
+        UserId = userId ?? throw new ArgumentNullException(nameof(userId));
+        ExpiresAt = expiresAt;
+        Status = ReservationStatus.Active;
+        CreatedAt = DateTime.UtcNow;
+        TotalPrice = Money.Zero();
     }
 
     public long FlightId { get; private set; }
 
-    public Guid UserId { get; private set; }
+    public string UserId { get; private set; }
 
-    public DateTime? ExpiresAt { get; private set; }
+    public DateTime ExpiresAt { get; private set; }
 
-    public byte Status { get; private set; }
+    public ReservationStatus Status { get; private set; }
 
-    public decimal TotalPrice { get; private set; }
+    public Money TotalPrice { get; private set; } = Money.Zero();
 
     public DateTime CreatedAt { get; private set; } = DateTime.UtcNow;
 
     private readonly List<long> _seatsIds = new();
     public IReadOnlyCollection<long> SeatsIds => _seatsIds.AsReadOnly();
 
+    //EF Core
+    private Reservation() { }
 
+    public static Reservation Create(long flightId, string userId, DateTime expiresAt)
+    {
+        if (flightId <= 0)
+            throw new ArgumentNullException("Flight Identity is not valid!", nameof(flightId));
+
+        if (string.IsNullOrWhiteSpace(userId))
+            throw new ArgumentNullException("User Identity is not valid!", nameof(userId));
+
+        if (expiresAt <= DateTime.UtcNow)
+            throw new ApplicationException("Expiration date must be in the future!");
+
+        return new Reservation(Guid.NewGuid(), flightId, userId, expiresAt);
+    }
 }
