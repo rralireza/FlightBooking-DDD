3e816e81c8fa5a368361a17b1dbdc0cc06a315d5
Alireza Nikandish <rralireza254@yahoo.com>
2025-12-18 10:36:24 +0330

    add inital Reservation aggregating root

---
+++ FlightBooking.Domain/Reservations/Reservation.cs
@@ -0,0 +1,29 @@
+﻿using FlightBooking.Domain.Abstractions;
+using FlightBooking.Domain.Seats;
+using System.Net;
+
+namespace FlightBooking.Domain.Reservations;
+
+public sealed class Reservation : Entity<Guid>
+{
+    private Reservation(Guid id) : base(id)
+    {
+    }
+
+    public long FlightId { get; private set; }
+
+    public Guid UserId { get; private set; }
+
+    public DateTime? ExpiresAt { get; private set; }
+
+    public byte Status { get; private set; }
+
+    public decimal TotalPrice { get; private set; }
+
+    public DateTime CreatedAt { get; private set; } = DateTime.UtcNow;
+
+    private readonly List<long> _seatsIds = new();
+    public IReadOnlyCollection<long> SeatsIds => _seatsIds.AsReadOnly();
+
+
+}
---
+++ FlightBooking.Domain/Reservations/ReservationStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace FlightBooking.Domain.Reservations;
+
+public enum ReservationStatus : byte
+{
+    Active = 1,
+    Expired = 2,
+    ConvertedToTicket = 3,
+    Cancelled = 4
+}
